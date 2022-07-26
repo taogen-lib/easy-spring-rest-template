32dcc51485acfd249a9554d7a160a4ef7160c913
taogen <taogenjia@gmail.com>
2022-07-26 13:36:39 +0800

    First commit

---
+++ src/test/java/com/taogen/easyresttemplate/App.java
@@ -0,0 +1,14 @@
+package com.taogen.easyresttemplate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Taogen
+ */
+@SpringBootApplication
+public class App {
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
+    }
+}
---
+++ src/test/java/com/taogen/easyresttemplate/User.java
@@ -0,0 +1,18 @@
+package com.taogen.easyresttemplate;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+/**
+ * @author Taogen
+ */
+@Data
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+    private Long id;
+    private String name;
+}
